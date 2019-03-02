22d65b89fc57c5254f17ecb379521a06f89e3030
Amrit Prasad <amrit_prasad@mfe.berkeley.edu>
2019-03-01 21:33:20 -0800

    Calculated IRs

--- functions.py
+++ functions.py
@@ -5,9 +5,33 @@ Authors: Ms. Allocation
 """
 
 # Imports
+import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import statsmodels.formula.api as sm
+
+
+def makedir(folder, sub_folder=None):
+    """
+    Checks if the folder exists or not. If it doesn't, creates it.
+    If sub_folder isn't None, checks folder/sub_folder
+
+    Args:
+        folder (str)
+
+        sub_folder (str)
+    """
+    # folder='Plots'; sub_folder='Variance_Capture'
+    if sub_folder is not None:
+        folderpath = os.path.join(folder, sub_folder)
+    else:
+        folderpath = folder
+
+    if not os.path.exists(folderpath):
+        print('Creating folder %s' % folderpath)
+        os.makedirs(folderpath)
+    return None
 
 
 def load_data(region):
@@ -36,8 +60,14 @@ def load_data(region):
                            parse_dates=[0], index_col=[0])[reg_mapper[region]]
     val_df = pd.read_excel(filepath, sheet_name='HML FF', skiprows=18,
                            parse_dates=[0], index_col=[0])[reg_mapper[region]]
+    rf_df = pd.read_excel(filepath, sheet_name='RF', skiprows=18,
+                          parse_dates=[0], index_col=[0])['Risk Free Rate']
     data_df = pd.concat([mkt_df.rename('MKT'), val_df.rename('VAL'),
-                         mom_df.rename('MOM'), qual_df.rename('QUAL')], axis=1)
+                         mom_df.rename('MOM'), qual_df.rename('QUAL'),
+                         rf_df.rename('RF')], axis=1)
+
+    # Drop dates with NaN RF
+    data_df.dropna(subset=['RF'], inplace=True)
     # Drop dates with all NaNs
     data_df.dropna(how='all', inplace=True)
     # Check that returns are all valid after the first valid index
@@ -59,7 +89,7 @@ def rolling_corr(data_df, corrwith, window, title):
     Function to plot the rolling correlations wrt the corrwith column
 
     Args:
-        data_df (pd.DataFrame)
+        data_df (pd.DataFrame): contains returns
 
         corrwith (str): column name wrt which correlations would be calculated
 
@@ -70,6 +100,9 @@ def rolling_corr(data_df, corrwith, window, title):
     # corrwith='MKT'; window=126; title='US_MKT'
     corrof = data_df.columns.difference([corrwith])
     plot_folder = 'Plots'
+    sub_folder = 'Factor_Corr'
+    # Create Plots folder
+    makedir(plot_folder, sub_folder)
     plt.clf()
     data_df.rolling(window=window, min_periods=int(window/3)).corr(
             pairwise=True).loc[pd.IndexSlice[:, corrof], corrwith].unstack(
@@ -77,6 +110,52 @@ def rolling_corr(data_df, corrwith, window, title):
                                               title=title)
     if not os.path.exists(plot_folder):
         os.makedirs(plot_folder)
-    plt.savefig(os.path.join(plot_folder, title + '.png'))
+    plt.savefig(os.path.join(plot_folder, sub_folder, title + '.png'))
     plt.show()
     plt.close()
+
+
+def calc_ir(data_df, bench='MKT'):
+    """
+    Function to calculate annual IRs of all factors
+
+    Args:
+        data_df (pd.DataFrame): contains returns
+
+        bench (str): benchmark
+
+    Returns:
+        ir_df (pd.DataFrame): contains IRs wrt benchmark
+    """
+    # bench='MKT'; bench='RF'
+    irof = data_df.columns.difference([bench])
+    reg_cols = [[x, bench] for x in irof]
+    ann_dates_end = pd.date_range(data_df.index.min(), data_df.index.max(),
+                                  freq='Y')
+    ann_dates_end = ann_dates_end.union([data_df.index.max() +
+                                         pd.offsets.YearEnd()])
+    ann_dates_start = pd.date_range(data_df.index.min(), data_df.index.max(),
+                                    freq='YS')
+    ann_dates_start = ann_dates_start.union([pd.offsets.YearBegin(-1) +
+                                             data_df.index.min()])
+    ir_df = pd.DataFrame(columns=irof)
+    for start_date, end_date in zip(ann_dates_start, ann_dates_end):
+        for col in reg_cols:
+            reg_df = data_df.loc[start_date:end_date, col].copy()
+            # Drop NAs
+            reg_df.dropna(inplace=True)
+            if reg_df.empty:
+                continue
+            else:
+                result = sm.ols('%s ~ %s' % (col[0], bench),
+                                data=reg_df).fit()
+                omega = np.sqrt(sum(result.resid**2))
+                alpha = result.params['Intercept']
+                if not np.allclose(omega, 0):
+                    # Set IR = alpha/omega if sum of squared residuals > 0
+                    ir_df.loc[end_date, col[0]] = float(alpha/omega)
+                else:
+                    ir_df.loc[end_date, col[0]] = np.nan
+    # Plot IRs
+    ir_df.plot(grid=True, figsize=(10, 6))
+    return ir_df.astype(float)
--- main.py
+++ main.py
@@ -9,11 +9,26 @@ import pandas as pd
 
 # Import functions
 import functions as fnc
+
+# Set options
+pd.set_option('display.max_columns', 20)
 # %%
-# Read in data
-us_df = fnc.load_data('US')
+# Read in data for US
+# us_df = fnc.load_data('US')
+us_df = pd.read_csv('./Data/US.csv')
 # Look at rolling correlations of factors vs the other
-fnc.rolling_corr(us_df, corrwith='MKT', window=126, title='US_MKT')
+fnc.rolling_corr(us_df, corrwith='MKT', window=126, title='US_MKT')  # ICs
 fnc.rolling_corr(us_df, corrwith='VAL', window=126, title='US_VAL')
 fnc.rolling_corr(us_df, corrwith='MOM', window=126, title='US_MOM')
 fnc.rolling_corr(us_df, corrwith='QUAL', window=126, title='US_QUAL')
+# %%
+# Calculate IRs
+# IRs wrt Market
+ir_mkt_df = fnc.calc_ir(us_df, bench='MKT')
+# IRs wrt Risk-Free (basically Sharpe)
+ir_rf_df = fnc.calc_ir(us_df, bench='RF')
+# The IRs wrt the market are very low for the factors implying that their
+# residual returns are usually not very significant. The IR for the risk-free
+# vs the Mkt is upward-biased because it doesn't change much during the year
+# causing its residual risk (std deviation) to be very low. It shouldn't be
+# taken as indicative of great performance.
